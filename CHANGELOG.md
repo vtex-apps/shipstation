b9d42baeccccb5a1e062af50508d6c7b940dc6e2
Brian Talma <Brian.Talma@vtex.com.br>
2020-11-02 07:27:29 -0500

    admin page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.0.5] - 2020-10-30
+
+### Added
+
+- Admin page
+
 ## [0.0.4] - 2020-10-19
 
 ### Changed
--- react/Admin.tsx
+++ react/Admin.tsx
@@ -2,60 +2,51 @@
 /* eslint-disable no-console */
 import React, { FC } from 'react'
 //import { useRuntime } from 'vtex.render-runtime'
-import axios from 'axios'
+//import axios from 'axios'
 import {
-  Layout,
-  PageHeader,
-//  Card,
-  Button,
-//  ButtonPlain,
-//  Divider,
-//  Spinner,
+    Layout,
+    PageHeader,
+    //  Card,
+    Button,
+    //  ButtonPlain,
+    //  Divider,
+    //  Spinner,
 } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
-
-//import styles from './styles.css'
-
 import { useQuery } from 'react-apollo'
-import Settings from '../graphql/AppSettings.graphql'
-
-const { data } = useQuery(Settings)
-const { apiKey } = JSON.parse(data?.appSettings?.ApiKey)
-const { apiSecret } = JSON.parse(data?.appSettings?.ApiSecret)
-
+import Settings from './graphql/AppSettings.graphql'
 const SETUP_HOOKS_URL = '/ship-station/setup-hooks'
 //const SYNCH_ORDER_URL = '/ship-station/synch-vtex-order/'
 //const LIST_HOOKS_URL = '/ship-station/list-active-webhooks'
-
-const setupHooks = () => {
-    axios
-        .get(SETUP_HOOKS_URL)
-}
-
 const ShipStationAdmin: FC = () => {
-  return (
-    <Layout
-      pageHeader={
-        <PageHeader
-          title={<FormattedMessage id="admin/ship-station.title" />}
-        />
-      }
-      >
-          <div>
-              Api Key {apiKey}
-              Api Secret {apiSecret}
-          <Button
-              variation="primary"
-              collapseLeft
-              onClick={() => {
-                  setupHooks()
-              }}
-          >
-              <FormattedMessage id="admin/ship-station.setup-hooks.button" />
-              </Button>
-              </div>
-    </Layout>
-  )
-}
+    const { data } = useQuery(Settings)
+    const setupHooks = () => {
+        fetch(SETUP_HOOKS_URL)
+    }
+    //const listHooks = () => {return JSON.stringify(fetch(LIST_HOOKS_URL))}
 
+    if (!data) return null
+    //const { apiKey, apiSecret } = JSON.parse(data.appSettings?.message || '{}')
+    return (
+        <Layout
+            pageHeader={
+                <PageHeader
+                    title={<FormattedMessage id="admin/ship-station.title" />}
+                />
+            }
+        >
+            <div>
+                <Button
+                    variation="primary"
+                    collapseLeft
+                    onClick={() => {
+                        setupHooks()
+                    }}
+                >
+                    <FormattedMessage id="admin/ship-station.setup-hooks.button" />
+                </Button>
+            </div>
+        </Layout>
+    )
+}
 export default ShipStationAdmin
